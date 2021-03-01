3b1a9879f7d48e1973eb4a99de1c6db9afc51f07
Felicia Heiney <bhfelicia@gmail.com>
2021-02-28 21:49:53 -0500

    famous quote rendering, WIP

--- src/EmployeeList.js
+++ src/EmployeeList.js
@@ -7,20 +7,30 @@ class EmployeeList extends Component {
     super();
     this.state = {
       employeeList: [],
+      employeeId: "",
     };
   }
   async componentDidMount() {
     const employeeList = (await axios.get("/api/employees")).data;
     this.setState({ employeeList });
+    window.addEventListener("hashchange", () => {
+      const employeeId = window.location.hash.slice(1);
+      console.log(employeeId);
+      this.setState({ employeeId: employeeId });
+    });
+    this.setState({ selectedUserId: window.location.hash.slice(1) });
   }
   render() {
-    const { employeeList } = this.state;
+    const { employeeList, employeeId } = this.state;
     return employeeList.map((employee) => {
       return (
         <div key={employee.id}>
           {" "}
           <a href={`#${employee.id}`}>{employee.name}</a>
           {employee.bossId === null ? ", Aka the Big Boss" : ""}
+          <div>
+            {!!employeeId && <SingleEmployee employeeId={employeeId} />}
+          </div>
         </div>
       );
     });
--- src/SingleEmployee.js
+++ src/SingleEmployee.js
@@ -1,11 +1,23 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class SingleEmployee extends Component {
   constructor() {
     super();
+    this.state = {
+      employee: {},
+    };
+  }
+  async componentDidMount() {
+    const employee = (
+      await axios.get(`/api/employees/${this.props.employeeId}`)
+    ).data;
+    this.setState({ employee });
   }
   render() {
-    return <hr></hr>;
+    const { employee } = this.state;
+    console.log(employee);
+    return <div>Famous quote: {employee.famousQuote}</div>;
   }
 }
 
